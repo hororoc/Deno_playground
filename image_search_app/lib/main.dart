0340e90754466938a2281af75b0eb9cf854d7a41
Min Jae Kim <exploit017@gmail.com>
2021-12-16 15:08:46 +0900

    불필요한 주석 제거

--- image_search_app/lib/main.dart
+++ image_search_app/lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:image_search_app/ui/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
